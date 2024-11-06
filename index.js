e25470283409f12dca172faf07d2e11465d5a70f
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2024-11-06 07:06:24 -0800

    added functionality for generating movies

--- index.js
+++ index.js
@@ -1,6 +1,58 @@
 const mainBody = document.getElementById("search-result-area")
+const form = document.getElementById("form")
+const search = document.getElementById("search")
+const apiKey = "13a9adac"
+let movies = []
+
 
 mainBody.innerHTML = `
     <img src="images/filmIcon.png">
     <h2>Start exploring</h2>
 `
+
+async function searchTitle(title) {
+    const res = await fetch(`http://www.omdbapi.com/?apikey=13a9adac&s=${title}`)
+    const data = await res.json()
+        movies = data.Search
+        getMovieResult()
+}
+
+async function getMovieResult() {
+    let htmlContent = ''
+    for (let movie of movies) {
+        const res = await fetch(`http://www.omdbapi.com/?apikey=13a9adac&i=${movie.imdbID}`)
+        const data = await res.json()
+        htmlContent += `
+            <div class="movie-container">
+                <img src="${data.Poster}">
+                <div class="movie-info-container">
+                    <div class="top-container">
+                        <h2 class="title">${data.Title}</h2>
+                        <p class="rating">${data.imdbRating}</p>
+                    </div>
+                    <div class="mid-container">
+                        <p class="runtime">${data.Runtime}</p>
+                        <p class="genre">${data.Genre}</p>
+                        <button class="add-btn">watchlist</button>
+                    </div>
+                    <p class="plot">${data.Plot}</p>
+                </div>
+            </div>
+        `
+    }
+    mainBody.innerHTML = htmlContent
+}
+
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    if (search.value) {
+        searchTitle(search.value)
+    } else {
+            mainBody.innerHTML = `
+        <img src="images/filmIcon.png">
+        <h2>Start exploring</h2>
+        `
+    }
+})
